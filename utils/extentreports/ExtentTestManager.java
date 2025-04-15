52033c0be398ffaaab74cd4bded2338ca71d3386
HoangVuNguyenLuong_164 <92324339+tonnipp@users.noreply.github.com>
2025-04-15 15:29:36 +0700

    pw: Implement Ultils for POM
    Implement Ultils for report and log tracking

---
+++ utils/extentreports/ExtentTestManager.java
@@ -0,0 +1,53 @@
+package utils.extentreports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import Core.BaseSetup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExtentTestManager {
+    static Map<Integer, ExtentTest> extentTestMap = new HashMap<>();
+    static ExtentReports extent = ExtentManager.createExtentReports();
+
+    public static synchronized ExtentTest getTest() {
+        return extentTestMap.get((int) Thread.currentThread().getId());
+    }
+
+    public static synchronized ExtentTest saveToReport(String testName, String desc) {
+        ExtentTest test = extent.createTest(testName, desc);
+        extentTestMap.put((int) Thread.currentThread().getId(), test);
+        return test;
+    }
+    public WebDriver getDriver() {
+        WebDriver driver = BaseSetup.getDriver();
+        return driver;
+    }
+
+    public static void addScreenShot(String message) {
+        String base64Image = "data:image/png;base64,"
+                + ((TakesScreenshot) BaseSetup.getDriver()).getScreenshotAs(OutputType.BASE64);
+        getTest().log(Status.INFO, message,        MediaEntityBuilder.createScreenCaptureFromBase64String(base64Image).build());
+    }
+
+    public static void addScreenShot(Status status, String message) {
+
+        String base64Image = "data:image/png;base64,"
+                + ((TakesScreenshot) BaseSetup.getDriver()).getScreenshotAs(OutputType.BASE64);
+        getTest().log(status, message,               MediaEntityBuilder.createScreenCaptureFromBase64String(base64Image).build());
+    }
+
+    public static void logMessage(String message) {
+        getTest().log(Status.INFO, message);
+    }
+
+    public static void logMessage(Status status, String message) {
+        getTest().log(status, message);
+    }
+}
